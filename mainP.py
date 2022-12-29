d3c3533d5ed05d86396a181413f4ddbfa045b2a2
Mehdi Badami <98187755+AliMehdi01@users.noreply.github.com>
2022-12-29 23:31:05 +0500

    Update mainP.py

--- mainP.py
+++ mainP.py
@@ -3,7 +3,7 @@ import time
 time.sleep(4)
 
 count = 0
-while count <=10
-    pyautogui.typewrite("zohaibXD" + str(count))
+while count <=10:
+    pyautogui.typewrite("virual" + str(count))
     count++
     pyautogui.press("enter")
