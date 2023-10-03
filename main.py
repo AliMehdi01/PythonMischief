fde5dc089af1c77aea6830dcca91167467dc5d57
Mehdi Badami <98187755+AliMehdi01@users.noreply.github.com>
2023-10-04 01:17:58 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import pyautogui
 import time
-time.sleep(4)
+import sys
 
+# Check if at least two arguments are provided (script name and input value)
+if len(sys.argv) < 2:
+    print("Usage: python file.py <input_value>")
+    print("Example: python file.py 100")
+    sys.exit(1)  # Exit with an error code
+# Access the command-line argument (assuming the input value is the second argument)
+input_value = int(sys.argv[1])
+
+# stop for 5s
+print("The Script is Started running...")
+time.sleep(5)
 count = 0
-while count <=10:
-    pyautogui.typewrite("text here" + str(count))
-    count++
+i = input_value
+while count <= i:
+    pyautogui.typewrite("Dumpshit" + str(count))
+    time.sleep(0.1)
+    count = count + 1
     pyautogui.press("enter")
